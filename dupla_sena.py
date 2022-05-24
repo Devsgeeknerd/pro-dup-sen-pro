791cb8f058ae331ec4b182a565d3f4df1d29d88a
Devsgeeknerd <vanderley.1109@gmail.com>
2022-05-24 14:39:42 -0300

    adicionando print

--- dupla_sena.py
+++ dupla_sena.py
@@ -34,3 +34,4 @@ print('-=' * 6, f'SORTEANDO {quantidade} JOGOS ', '-=' * 6)
 for i, l in enumerate(jogos):
     print(f' Jogo {i+1}: {l}')
     sleep(1)
+print('-=' * 9, '< BOA SORTE!!! >', '-=' * 9)
