5414d159066844a832fdf76979c953b0968984d4
Paulo <vanderley.1109@gmail.com>
2024-01-22 15:46:30 -0300

    atualizando conteudo

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 <!-- Título -->
 # Gerador de Bilhetes Para Dupla Sena
 
+> [!Important]\
+> **Boas práticas e sugestões de melhoria**:
+>
+> * Adicione uma validação para garantir que o usuário insira um valor positivo para a quantidade de bilhetes.
+> * Se os valores 1 e 50 são constantes que não mudarão, considere declará-los como constantes no início do código.
+> * Considere encapsular a lógica de geração de bilhetes em uma função separada para tornar o código mais modular.
+> * Adicione comentários explicativos em seções complexas do código para facilitar a compreensão.
+> * Informe o usuário sobre as restrições dos números (1 a 50) durante a entrada.
+> * Considere adicionar tratamento de erros para casos em que a quantidade de bilhetes desejada seja maior do que a combinação possível de números únicos.
+
 <!-- Informações -->
 ## &#8505; Informações
 
--- dupla_sena.py
+++ dupla_sena.py
@@ -1,23 +1,63 @@
-# Módulos
 from random import randint
 from time import sleep
 
-# Lista
+# Inicialização de listas vazias
 lista = list()
-
-# Jogos
 jogos = list()
 
-# Título
+# Apresentação do título do programa
 print('-' * 15)
 print('        JOGA NA DUPLA SENA        ')
 print('-' * 15)
 
-# Pergunta
+# Entrada do usuário para a quantidade de bilhetes a serem gerados
+quantidade = int(input('Quantos bilhetes quer gerar? '))
+total = 1  # Inicialização do contador de bilhetes gerados
+
+# Loop para gerar os bilhetes
+while total <= quantidade:
+    cont = 0  # Inicialização do contador de números únicos gerados
+    numero = randint(1, 50)  # Gera um número aleatório entre 1 e 50
+
+    # Verifica se o número gerado não está na lista
+    if numero not in lista:
+        lista.append(numero)  # Adiciona o número à lista
+        cont += 1  # Incrementa o contador de números únicos
+
+    # Se já foram gerados 6 números únicos, encerra o loop
+    if cont >= 6:
+        break
+
+    lista.sort()  # Ordena a lista de números
+    jogos.append(lista[:])  # Adiciona uma cópia da lista de números ao conjunto de jogos
+    lista.clear()  # Limpa a lista para gerar um novo bilhete
+    total += 1  # Incrementa o contador de bilhetes gerados
+
+# Apresentação dos bilhetes gerados
+print('-=' * 6, f'SORTEANDO {quantidade} JOGOS ', '-=' * 6)
+for i, l in enumerate(jogos):
+    print(f' Jogo {i+1}: {l}')
+    sleep(1)  # Aguarda 1 segundo entre cada bilhete
+print('-=' * 9, '< BOA SORTE!!! >', '-=' * 9)
+
+from random import randint
+from time import sleep
+
+
+lista = list()
+
+
+jogos = list()
+
+
+print('-' * 15)
+print('        JOGA NA DUPLA SENA        ')
+print('-' * 15)
+
+
 quantidade = int(input('Quantos bilhetes quer gerar? '))
 total = 1
 
-# Sorteando os números
 while total <= quantidade:
     cont = 0
     numero = randint(1, 50)
